9f57ab2e000cb0dcfda0d9d0dacae13a658ac378
Prince <113298988+uixprince@users.noreply.github.com>
2026-04-05 11:21:17 +0530

    Update Index.py

--- api/Index.py
+++ api/Index.py
@@ -1,13 +1,7 @@
-ppfrom http.server import BaseHTTPRequestHandler
-import os
+from http.server import BaseHTTPRequestHandler
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        try:
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(b"WORKING OK")
-        except Exception as e:
-            self.send_response(500)
-            self.end_headers()
-            self.wfile.write(str(e).encode())
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
